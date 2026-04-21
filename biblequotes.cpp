739bae05adc469fba2b427d38019f19124e52c1b
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-21 11:16:31 -0500

    Create biblequotes.cpp
    setting up basic .cpp file to connect to .h file

---
+++ biblequotes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <cctype> // to use tolower
+#include "biblequotes.h"
+using namespace std;
+
+// sets up basic constructor
+Menu::Menu() {
+
+}
+
+// user requests rand quote
+int Menu::option1() {
+
+    return 0;
+}
+
+// user requests rand quote from specific book
+int Menu::option2() {
+
+    return 0;
+}
+
+// user requests all quotes from specific book
+int Menu::option3() {
+
+    return 0;
+}
