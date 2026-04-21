4a4cc7e16834032932c5c2509bb1fc0b90c8597f
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-21 10:55:20 -0500

    Create main.cpp
    setting up basic file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <random> // for outputting random quotes
+#include "biblequotes.h"
+using namespace std;
+
+int main() {
+
+    return 0;
+}
