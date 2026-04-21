007ecc5c9468bc5a358dba069bc01b7ead9c2d3d
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-21 11:04:01 -0500

    Create biblequotes.h
    setting up basic .h file

---
+++ biblequotes.h
@@ -0,0 +1,20 @@
+#ifindef BIBLE_QUOTES_H_
+#define BIBLE_QUOTES_H_
+
+#include <string> // might not need
+
+class Menu {
+    public:
+        // default constructor
+        Menu();
+
+        int option1();
+        int option2();
+        int option3();
+        void menuDisplay();
+
+    private:
+
+};
+
+#endif // BIBLE_QUOTES_H_
